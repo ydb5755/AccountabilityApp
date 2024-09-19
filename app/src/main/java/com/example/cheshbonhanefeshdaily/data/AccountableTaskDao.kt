11d11b29be3d7922a156d4815cfc299fbca7c957
yisroelBaum <yisroel.d.baum@gmail.com>
2024-09-19 13:15:25 +0300

    initializing room db

--- app/src/main/java/com/example/cheshbonhanefeshdaily/data/AccountableTaskDao.kt
+++ app/src/main/java/com/example/cheshbonhanefeshdaily/data/AccountableTaskDao.kt
@@ -9,7 +9,7 @@ import androidx.room.Update
 // when writing Queries, its good to specify a Flow<> as the return type, see step 13 in https://developer.android.com/codelabs/basic-android-kotlin-compose-persisting-data-room?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-6-pathway-2%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-persisting-data-room#5
 
 
-@Dao 
+@Dao
 interface AccountableTaskDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
